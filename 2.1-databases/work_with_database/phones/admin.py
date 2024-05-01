731a4687bd0597f7ec4afb05535facf74c4cdb78
EkaterinaGor4 <147917969+EkaterinaGor4@users.noreply.github.com>
2024-05-01 20:21:35 +0300

    Add files via upload

--- 2.1-databases/work_with_database/phones/admin.py
+++ 2.1-databases/work_with_database/phones/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+@admin.register(Phone)
+class PhoneAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'image', 'price', 'lte_exists', 'slug']
+    list_filter = ['name', 'price']
